a8916fe05d38cdb034f6ca10d628d60616e7e001
ColeBurch <48129706+ColeBurch@users.noreply.github.com>
2024-04-06 13:31:56 -0600

    Implement insertion sort in descending order.

--- Insertion_Sort.py
+++ Insertion_Sort.py
@@ -8,6 +8,16 @@ def insertion_sort(A):
         A[i + 1] = current
     return(A)
 
+def insertion_sort_descending(A):
+    for j in range(1, len(A)):
+        current = A[j]
+        i = j - 1
+        while i >= 0 and A[i] < current:
+            A[i + 1] = A[i]
+            i = i - 1
+        A[i + 1] = current
+    return(A)
+
 test_1 = [5, 2, 4, 6, 1 , 3]
 test_2 = [-1, 0, 1, 2]
 test_3 = [1, -1, 2, 0]
@@ -15,3 +25,7 @@ test_3 = [1, -1, 2, 0]
 print(insertion_sort(test_1))
 print(insertion_sort(test_2))
 print(insertion_sort(test_3))
+
+print(insertion_sort_descending(test_1))
+print(insertion_sort_descending(test_2))
+print(insertion_sort_descending(test_3))
